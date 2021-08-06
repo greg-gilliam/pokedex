20ebc5b24ba7e4865ed98de1ca067ba5d77970ce
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-06 14:01:07 -0700

    added sort

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import CharacterList from './CharacterList.js'
 
 
 class App extends Component {
-  state = { data: [], loading: true, query: null, sort: '' };
+  state = { data: [], loading: true, query: null, sortOrder: 'asc' };
+
+  componentDidMount() {
+    this.fetchData();
+  };
 
   fetchData = async() => {
     let url = "https://pokedex-alchemy.herokuapp.com/api/pokedex";
+    let searchParams = new URLSearchParams();
+
+
     if(this.state.query){
-      url = url + `?pokemon=${this.state.query}`;
+      searchParams.set('pokemon', this.state.query);
     }
+    if (this.state.sortOrder) {
+      searchParams.set('sort', 'pokemon');
+      searchParams.set('direction', this.state.sortOrder);
+    }
+      // url = url + `?pokemon=${this.state.query}`;
+    
+
+    url = url + `?${searchParams.toString()}`;
+
     let response = await fetch(url);
     let data = await response.json();
 
@@ -20,9 +36,6 @@ class App extends Component {
   updateQuery = (event)=>{
    this.setState({ query: event.target.value });
 }
-componentDidMount() {
-  this.fetchData();
-};
 
   render() { 
     return(
@@ -31,6 +44,10 @@ componentDidMount() {
         {this.state.loading && <h3> LOADING!! </h3>}
         {!this.state.loading && (
           <section>
+            <select onChange={this.updateSort}>
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
             <input onChange={this.updateQuery} type="text"></input>
             <button onClick={this.fetchData}>Search!</button>
             <CharacterList characters={this.state.data} />
