4844d185a809f063be3069e53d800cbab338f10a
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-05 17:21:27 -0700

    working search function

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import CharacterList from './CharacterList.js'
 
 
 class App extends Component {
-  state = { data: [], loading: true };
+  state = { data: [], loading: true, query: null };
 
   fetchData = async() => {
     let url = "https://pokedex-alchemy.herokuapp.com/api/pokedex";
+    if(this.state.query){
+      url = url + `?pokemon=${this.state.query}`;
+    }
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
 
     this.setState({ data: data.results, loading: false });
   };
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  updateQuery = (event)=>{
+   this.setState({ query: event.target.value });
+}
+componentDidMount() {
+  this.fetchData();
+};
 
   render() { 
     return(
@@ -25,7 +30,11 @@ class App extends Component {
         <h1>Pokemon!!</h1>
         {this.state.loading && <h3> LOADING!! </h3>}
         {!this.state.loading && (
-          <CharacterList characters={this.state.data} />
+          <section>
+            <input onChange={this.updateQuery} type="text"></input>
+            <button onClick={this.fetchData}>Search!</button>
+            <CharacterList characters={this.state.data} />
+          </section>
         )}
       </>
     );
