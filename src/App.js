649e4610335bf77741a9dad36fe422d193db37b9
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-09 15:49:14 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { Component } from 'react';
-import './App.css';
-import PokeList from './PokeList.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import PokemonContainer from './PokemonContainer.js'
+import Home from './Home.js'
+import './App.css';
+import Header from './Header.js';
+
 
 
 class App extends Component {
   render() {
-   return <PokemonContainer />;
+   return (
+   <section className="app">
+     <BrowserRouter>
+     <Header />
+     <Switch>
+       {/* <Route path="/pokemon/:pokeId" component={PokeDetail} /> */}
+       <Route path="/pokemon" component={PokemonContainer} />
+       {/* <Route path="/" exact>
+        <h2>All the Pokemon!</h2>
+       </Route> */}
+       <Route path="/" component={Home} />
+     </Switch>
+     </BrowserRouter>
+   </section>
+   );
   }
 }
 
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+
+class Header extends Component {
+    render() { 
+        return ( 
+            <header>
+                <div>pokedex</div>
+                <div className="links">
+                    <NavLink exact to="/">Home</NavLink>
+                    <NavLink to="/pokemon">Pokemon List</NavLink>
+                </div>
+            </header>
+         );
+    }
+}
+ 
+export default Header;
